abafb899b5eec907ce4f65a8189b23e7ef12cd4d
Gabriel Simon <gabriel_simon1@estudante.sc.senai.br>
2025-09-23 19:03:30 -0300

    compra, item_compra e rotas

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { ativaDesativaUsuario as ativarDesativarUsuario, atualizarUsuario, criarUsuario, listarUsuarios } from "../controllers/UsuarioController";
 import { login } from "../controllers/LoginController";
 import { atualizarProduto, buscarProduto, cadastrarProduto, excluirProduto, listarProdutos } from "../controllers/ProdutoController";
+import { atualizarCompra, buscarCompra, cadastrarCompra, excluirCompra, listarCompras } from "../controllers/CompraController";
+import { atualizarItemCompra, buscarItemCompra, cadastrarItemCompra, excluirItemCompra, listarItensCompra } from "../controllers/ItemCompraController";
 
 const router = Router();
 
@@ -19,10 +21,18 @@ router.delete("/excluirProduto/:id", excluirProduto);
 router.get("/procurarProduto/:id", buscarProduto);
 router.get("/procurarProduto", buscarProduto);
 
+router.get("/listarCompras", listarCompras);
+router.post("/criarCompra", cadastrarCompra);
+router.put("/atualizarCompra/:id", atualizarCompra);
+router.delete("/excluirCompra/:id", excluirCompra);
+router.get("/procurarCompra/:id", buscarCompra);
+router.get("/procurarCompra", buscarCompra);
 
-//fazer pedido
-//listar pedidos
-//atualizar pedido
-//cancelar pedido(soft delete)
+router.get("/listarItensCompra/:compra_id", listarItensCompra);
+router.post("/criarItemCompra", cadastrarItemCompra);
+router.put("/atualizarItemCompra/:id", atualizarItemCompra);
+router.delete("/excluirItemCompra/:id", excluirItemCompra);
+router.get("/procurarItemCompra/:id", buscarItemCompra);
+router.get("/procurarItemCompra", buscarItemCompra);
 
 export default router;
